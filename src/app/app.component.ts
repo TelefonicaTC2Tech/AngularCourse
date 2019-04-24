91dbe06bcc9c7554f1138eeaff7fcce5c098586c
Ana <anabelengp@gmail.com>
2019-04-24 09:03:10 +0200

    feat: step1 - DI: inject singleton service in components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { QuestionService } from './question.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'AngularCourse';
+
+  constructor(private questionService: QuestionService) {}
+
+  ngOnInit(): void {
+    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
+    //Add 'implements OnInit' to the class.
+    this.questionService.initQuestions();
+  }
 }
