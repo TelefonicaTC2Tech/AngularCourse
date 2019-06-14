bbfa800b5f19902e33869e213f9654d5f80c079d
Ana Belén García Parra <anabelen.garciaparra@telefonica.com>
2019-06-14 07:46:12 +0200

    feat: step2 - map server response to application model by RxJs

--- src/app/question.service.spec.ts
+++ src/app/question.service.spec.ts
@@ -51,7 +51,7 @@ describe('QuestionService', () => {
     service.getQuestion().subscribe((question: Question) => {
       expect(question).toBeDefined();
       expect(question.question).toBeDefined();
-      expect(question.incorrect_answers).toBeDefined();
+      expect(question.answers).toBeDefined();
       expect(question.correct_answer).toBeDefined();
       done();
     });
@@ -80,7 +80,7 @@ describe('QuestionService', () => {
     // https://rxjs-dev.firebaseapp.com/guide/subscription
     forkJoin([question$, question2$]).subscribe(([q, q2]: Question[]) => {
       expect(q2.question).toBeDefined();
-      expect(q2.incorrect_answers).toBeDefined();
+      expect(q2.answers).toBeDefined();
       expect(q2.correct_answer).toBeDefined();
       expect(q2.question).not.toEqual(q.question);
       done();
