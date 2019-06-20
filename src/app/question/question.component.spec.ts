184ee3ff539b2ef82b77ab5e9a5ddc5ff0b03b61
Ana Belén García Parra <anabelen.garciaparra@telefonica.com>
2019-06-20 11:07:29 +0200

    feat: step3 - using interpolation, content Security preventing HTML interpretation

--- src/app/question/question.component.spec.ts
+++ src/app/question/question.component.spec.ts
@@ -1,16 +1,32 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { QuestionComponent } from './question.component';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { QuestionService } from '../question.service';
+import { Question } from '../question';
+import { of } from 'rxjs';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
 
 describe('QuestionComponent', () => {
   let component: QuestionComponent;
   let fixture: ComponentFixture<QuestionComponent>;
+  let questionEl: HTMLElement;
+
+  // mocks
+  const questionMultiple = {
+    category: 'General Knowledge',
+    type: 'multiple',
+    difficulty: 'easy',
+    question: 'Which of these colours is NOT featured in the logo for &quot;Google?&quot;',
+    correct_answer: 'Pink',
+    answers: ['Yellow', 'Blue', 'Pink', 'Green']
+  } as Question;
+  const questionServiceSpy = jasmine.createSpyObj('QuestionService', ['getQuestion']);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
-      declarations: [ QuestionComponent ]
+      declarations: [ QuestionComponent ],
+      schemas: [NO_ERRORS_SCHEMA],
+      providers: [ { provide: QuestionService, useValue: questionServiceSpy }]
     })
     .compileComponents();
   }));
@@ -18,10 +34,22 @@ describe('QuestionComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(QuestionComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
   });
 
   it('should create', () => {
+    questionServiceSpy.getQuestion.and.returnValue(of(questionMultiple));
+    fixture.detectChanges(); // onInit()
+
     expect(component).toBeTruthy();
+  });
+  it('should show a question', () => {
+    const expectedQuestion = 'Which of these colours is NOT featured in the logo for "Google?"';
+    const getQuestionSpy = questionServiceSpy.getQuestion.and.returnValue(of(questionMultiple));
+    fixture.detectChanges(); // onInit()
+
+    questionEl = fixture.nativeElement.querySelector('.question-text');
+    // sync spy result shows question immediately after init
+    expect(questionEl.textContent).toBe(expectedQuestion);
+    expect(getQuestionSpy.calls.any()).toBe(true, 'getQuestion called');
   });
 });
