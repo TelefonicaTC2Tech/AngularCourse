d8ad1794ed3504e22c0e645ba651507e8c6e152a
Ana Belén García Parra <anabelen.garciaparra@telefonica.com>
2019-09-20 13:03:17 +0200

    feat: step4 - link to other routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TrivialComponent } from './trivial/trivial.component';
+import { ContentAttributionComponent } from './content-attribution/content-attribution.component';
 
 const routes: Routes = [
   { path: '', component: TrivialComponent },
+  { path: 'attributions', component: ContentAttributionComponent },
   { path: '**', redirectTo: '' }
 ];
 
