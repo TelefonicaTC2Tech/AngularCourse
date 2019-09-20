113f9806ca1bbb72f80cb2758d32fd0878742228
Ana Belén García Parra <anabelen.garciaparra@telefonica.com>
2019-09-20 13:03:17 +0200

    feat: step4 - link to other routes

---
+++ src/app/content-attribution/content-attribution.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-content-attribution',
+  templateUrl: './content-attribution.component.html',
+  styleUrls: ['./content-attribution.component.scss']
+})
+export class ContentAttributionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
