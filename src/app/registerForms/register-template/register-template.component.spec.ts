eabd5535302b11115b4ebf30d3eebc2c2a686113
Carlos Lobo Mata <carlos.lobomata@telefonica.com>
2019-10-14 16:32:31 +0200

    feat: step 5- adding built-in validators, errors and tests

--- src/app/registerForms/register-template/register-template.component.spec.ts
+++ src/app/registerForms/register-template/register-template.component.spec.ts
@@ -66,4 +66,100 @@ describe('RegisterTemplateComponent', () => {
       expect(hostElement.querySelector('#register-btn').classList.contains('disabled')).toBeFalsy();
     });
   });
+  it('should show errors in name field when neccesary', () => {
+    // Name field is mandatory and needs at least 5 characters
+    // Errors will not show if the field isnt touched
+    // whenStable is needed to work with ngForm and template forms
+    fixture.whenStable().then(() => {
+      expect(hostElement.querySelector('#username-error')).toBeFalsy();
+      const nameField = hostElement.querySelector('#username') as HTMLInputElement
+      nameField.dispatchEvent(new Event('blur'));
+      fixture.detectChanges();
+      expect(hostElement.querySelector('#username-error')).toBeTruthy();
+      nameField.value = "username"
+      nameField.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      expect(hostElement.querySelector('#username-error')).toBeFalsy();
+      nameField.value = "user"
+      nameField.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      expect(hostElement.querySelector('#username-error')).toBeTruthy();
+      nameField.value = "valid"
+      nameField.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      expect(hostElement.querySelector('#username-error')).toBeFalsy();
+      nameField.value = "verylongusername"
+      nameField.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      expect(hostElement.querySelector('#username-error')).toBeFalsy();
+      nameField.value = ""
+      nameField.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      expect(hostElement.querySelector('#username-error')).toBeTruthy();
+    });
+  });
+  it('should show errors in email field when neccesary', () => {
+    // Email field is mandatory and needs to ve a correct email
+    // Errors will not show if the field isnt touched
+    // whenStable is needed to work with ngForm and template forms
+    fixture.whenStable().then(() => {
+      expect(hostElement.querySelector('#email-error')).toBeFalsy();
+      const emailField = hostElement.querySelector('#email') as HTMLInputElement
+      emailField.dispatchEvent(new Event('blur'));
+      fixture.detectChanges();
+      expect(hostElement.querySelector('#email-error')).toBeTruthy();
+      emailField.value = "validemail@valid.es"
+      emailField.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      expect(hostElement.querySelector('#email-error')).toBeFalsy();
+      emailField.value = "notvalidemail"
+      emailField.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      expect(hostElement.querySelector('#email-error')).toBeTruthy();
+      emailField.value = "a@a.a"
+      emailField.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      expect(hostElement.querySelector('#email-error')).toBeFalsy();
+      emailField.value = "verylonguseremail@very.long"
+      emailField.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      expect(hostElement.querySelector('#email-error')).toBeFalsy();
+      emailField.value = ""
+      emailField.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      expect(hostElement.querySelector('#email-error')).toBeTruthy();
+    });
+  });
+  it('should show errors in password field when neccesary', () => {
+    // Password field is mandatory and needs at least 5 characters 
+    // Errors will not show if the field isnt touched
+    // whenStable is needed to work with ngForm and template forms
+    fixture.whenStable().then(() => {
+      expect(hostElement.querySelector('#password-error')).toBeFalsy();
+      const passwordField = hostElement.querySelector('#password') as HTMLInputElement
+      passwordField.dispatchEvent(new Event('blur'));
+      fixture.detectChanges();
+      expect(hostElement.querySelector('#password-error')).toBeTruthy();
+      passwordField.value = "password"
+      passwordField.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      expect(hostElement.querySelector('#password-error')).toBeFalsy();
+      passwordField.value = "pass"
+      passwordField.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      expect(hostElement.querySelector('#password-error')).toBeTruthy();
+      passwordField.value = "valid"
+      passwordField.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      expect(hostElement.querySelector('#password-error')).toBeFalsy();
+      passwordField.value = "verylongpassword"
+      passwordField.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      expect(hostElement.querySelector('#password-error')).toBeFalsy();
+      passwordField.value = ""
+      passwordField.dispatchEvent(new Event('input'));
+      fixture.detectChanges();
+      expect(hostElement.querySelector('#password-error')).toBeTruthy();
+    });
+  });
 });
